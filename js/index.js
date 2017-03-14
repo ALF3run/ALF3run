87233ed4dc6da312d10ad82e38ba1ac20d7f8420
Andrea Filippo Vittorio La Fico Guzzo <alf4pk@gmail.com>
2017-03-14 03:35:26 +0100

    Update index.js
    - Added entrance animation.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
-  $('h1').addClass("animated fadeIn");
-  $('h1 small').addClass("animated fadeIn");
-  $('.social').addClass("animated fadeIn");
+  $('#title-box').css("visibility", "visible").addClass("animated fadeIn");
   $('a').click(function(){
     $(this).blur();
+  });
+  
+  $(window).scroll(function() {
+    if($(window).scrollTop() > Math.min($(window).height(), $(window).width())*0.6) {
+      $('#about').css("visibility", "visible").addClass("animated fadeInUp");
+      $('#skills').css("visibility", "visible").addClass("animated rotateInUpRight");
+      $(window).off();
+    }
   });
 });
