d402c64992dfabae1372260838a59a10f9dd6106
Andrea Filippo Vittorio La Fico Guzzo <alf4pk@gmail.com>
2018-10-29 15:40:53 +0100

    Progress bar sliding
    Changed some animations

--- js/index.js
+++ js/index.js
@@ -6,17 +6,25 @@ $(document).ready(function() {
   
   $(window).scroll(function() {
     if($(window).scrollTop() > $('#sec2').offset().top-Math.min($(window).width(),$(window).height())*0.8 && $(window).width() >= 992) {
-      $('#about').css("visibility", "visible").addClass("animated fadeInUp");
-      $('#skills').css("visibility", "visible").addClass("animated rotateInUpRight");
+      $('#about').css("visibility", "visible").addClass("animated fadeInLeft");
+      $('#skills').css("visibility", "visible").addClass("animated fadeInRight");
+      setTimeout(fillingProgressBars, 500); 
     } 
     if($(window).scrollTop() > $('#sec2').offset().top-Math.min($(window).width(),$(window).height())*0.8 && $(window).width() < 992) {
-      $('#about').css("visibility", "visible").addClass("animated fadeInUp");
+      $('#about').css("visibility", "visible").addClass("animated fadeInLeft");
     } 
     if($(window).scrollTop() > $('#skills').offset().top-Math.min($(window).width(),$(window).height())*0.8  && $(window).width() <992) {
-      $('#skills').css("visibility", "visible").addClass("animated fadeInUp");
-    } 
+      $('#skills').css("visibility", "visible").addClass("animated fadeInLeft");
+      setTimeout(fillingProgressBars, 500); 
     if($(window).scrollTop() > $('.thumbnail').offset().top-Math.min($(window).width(),$(window).height())*0.8) {
       $('.thumbnail').css("visibility", "visible").addClass("animated fadeInUp");
     }
   });
 });
+
+function fillingProgressBars() {
+  $('progress-bar')[0].style.width = "90%";
+  $('progress-bar')[0].style.width = "80%";
+  $('progress-bar')[0].style.width = "90%";
+  $('progress-bar')[0].style.width = "80%";
+}
